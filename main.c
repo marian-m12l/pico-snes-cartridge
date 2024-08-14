bf4c9f65fbc15add5d63482ab300bed3255097c0
marian <marian.muller@serli.com>
2024-08-14 23:07:19 +0200

    use rp2350 + add support for bank address

--- main.c
+++ main.c
@@ -10,46 +10,75 @@
 #define COUNTER_THRESHOLD 10000000 // FIXME 140000
 #define BUFFER_SIZE 20000
 uint32_t counter = 0;
-uint16_t addresses[BUFFER_SIZE];
+uint32_t addresses[BUFFER_SIZE];
 uint8_t datas[BUFFER_SIZE];
 uint32_t datas_out[BUFFER_SIZE];
 #endif
+
+#define SNES_ADDR_PINS_MASK  0x0000000000ffffff
+#define SNES_DATA_PINS_MASK  0x00000000ff000000
+#define SNES_DATA_PINS_SHIFT 24
+#define SNES_RD_PIN_MASK     0x0000000100000000
+#define SNES_CART_PIN_MASK   0x0000000200000000
+#define SNES_CTRL_PINS_MASK  (SNES_RD_PIN_MASK | SNES_CART_PIN_MASK)
+
+#define SNES_ALL_PINS_MASK (SNES_ADDR_PINS_MASK | SNES_DATA_PINS_MASK | SNES_CTRL_PINS_MASK)
+
+uint32_t map_address_to_rom(uint32_t address) {
+    // FIXME ADDRESS IS 24 BITS LONG --> HANDLE BANK ADDRESS CORRECTLY DEPENDING ON MEMORY MAPPER ??
+    uint32_t bank = address >> 16;
+    // FIXME LoROM / HiROM / ExHiROM ??? --> Check rom type at 0x7fd5 ??
+    // TODO DON'T READ ROMTYPE EVERYTIME !!!
+    uint8_t romtype = rom[0x7fd5] & 0x0f;  // 0: LoROM, 1: HiROM, 5: ExHiROM
+    // FIXME (rom[0x7fd5] & 0x10) indicates SLOW or FAST ROM --> always patch to SLOW ??
+    // FIXME address & 0x7fff or address & 0x6fff depending on (address & 0xf000) == 0xf000 ???
+    if (romtype == 0) { // LoROM
+        // LoROM: Up to 128 32KB-banks. Bank indexing starting from 0x80. Each bank starts at 0x8000
+        return (bank & 0x7f) * 32768 + (address & 0x7fff);
+    } else if (romtype == 1) {  // HiROM
+        // HiROM: Up to 64 64KB-banks. Bank indexing starting from 0xc0. Each bank starts at 0x0000
+        return (bank & 0x3f) * 65536 + address;
+    } else {
+        // TODO Add support for ExHiROM
+        return (bank & 0x3f) * 65536 + address;
+    }
+}
 
 int main() {
     stdio_init_all();
 
     // Overclock
-    vreg_set_voltage(VREG_VOLTAGE_1_20);
-    set_sys_clock_khz(284000, true);
+    //vreg_set_voltage(VREG_VOLTAGE_1_20);
+    //set_sys_clock_khz(284000, true);
 
     // Configure GPIOs
-    gpio_init_mask(0x1800ffff | 0x047f0000);
-    gpio_set_dir_in_masked(0x1800ffff);
-    gpio_set_dir_out_masked(0x047f0000);
+    gpio_set_dir_masked64(SNES_ALL_PINS_MASK, 0x0000000000000000);
+    gpio_put_masked64(SNES_ALL_PINS_MASK, 0x0000000000000000);
+    gpio_set_function_masked64(SNES_ALL_PINS_MASK, GPIO_FUNC_SIO);
 
-    gpio_set_slew_rate(16, GPIO_SLEW_RATE_FAST);
-    gpio_set_slew_rate(17, GPIO_SLEW_RATE_FAST);
-    gpio_set_slew_rate(18, GPIO_SLEW_RATE_FAST);
-    gpio_set_slew_rate(19, GPIO_SLEW_RATE_FAST);
-    gpio_set_slew_rate(20, GPIO_SLEW_RATE_FAST);
-    gpio_set_slew_rate(21, GPIO_SLEW_RATE_FAST);
-    gpio_set_slew_rate(22, GPIO_SLEW_RATE_FAST);
-    gpio_set_slew_rate(26, GPIO_SLEW_RATE_FAST);
+    // FIXME Required ??
+    gpio_set_slew_rate(SNES_DATA_PINS_SHIFT, GPIO_SLEW_RATE_FAST);
+    gpio_set_slew_rate(SNES_DATA_PINS_SHIFT+1, GPIO_SLEW_RATE_FAST);
+    gpio_set_slew_rate(SNES_DATA_PINS_SHIFT+2, GPIO_SLEW_RATE_FAST);
+    gpio_set_slew_rate(SNES_DATA_PINS_SHIFT+3, GPIO_SLEW_RATE_FAST);
+    gpio_set_slew_rate(SNES_DATA_PINS_SHIFT+4, GPIO_SLEW_RATE_FAST);
+    gpio_set_slew_rate(SNES_DATA_PINS_SHIFT+5, GPIO_SLEW_RATE_FAST);
+    gpio_set_slew_rate(SNES_DATA_PINS_SHIFT+6, GPIO_SLEW_RATE_FAST);
+    gpio_set_slew_rate(SNES_DATA_PINS_SHIFT+7, GPIO_SLEW_RATE_FAST);
 
-    gpio_set_drive_strength(16, GPIO_DRIVE_STRENGTH_8MA);
-    gpio_set_drive_strength(17, GPIO_DRIVE_STRENGTH_8MA);
-    gpio_set_drive_strength(18, GPIO_DRIVE_STRENGTH_8MA);
-    gpio_set_drive_strength(19, GPIO_DRIVE_STRENGTH_8MA);
-    gpio_set_drive_strength(20, GPIO_DRIVE_STRENGTH_8MA);
-    gpio_set_drive_strength(21, GPIO_DRIVE_STRENGTH_8MA);
-    gpio_set_drive_strength(22, GPIO_DRIVE_STRENGTH_8MA);
-    gpio_set_drive_strength(26, GPIO_DRIVE_STRENGTH_8MA);
+    // FIXME Required ??
+    gpio_set_drive_strength(SNES_DATA_PINS_SHIFT, GPIO_DRIVE_STRENGTH_8MA);
+    gpio_set_drive_strength(SNES_DATA_PINS_SHIFT+1, GPIO_DRIVE_STRENGTH_8MA);
+    gpio_set_drive_strength(SNES_DATA_PINS_SHIFT+2, GPIO_DRIVE_STRENGTH_8MA);
+    gpio_set_drive_strength(SNES_DATA_PINS_SHIFT+3, GPIO_DRIVE_STRENGTH_8MA);
+    gpio_set_drive_strength(SNES_DATA_PINS_SHIFT+4, GPIO_DRIVE_STRENGTH_8MA);
+    gpio_set_drive_strength(SNES_DATA_PINS_SHIFT+5, GPIO_DRIVE_STRENGTH_8MA);
+    gpio_set_drive_strength(SNES_DATA_PINS_SHIFT+6, GPIO_DRIVE_STRENGTH_8MA);
+    gpio_set_drive_strength(SNES_DATA_PINS_SHIFT+7, GPIO_DRIVE_STRENGTH_8MA);
 
-    gpio_set_dir_in_masked(0x047f0000);
-    gpio_clr_mask(0x047f0000);    
 
     printf("Waiting for SNES to boot...\n");
-    while(((sio_hw->gpio_in) & 0x18000000) == 0) {
+    while((gpio_get_all64() & SNES_CTRL_PINS_MASK) == 0) {
         tight_loop_contents();
     }
 
@@ -69,26 +98,20 @@ int main() {
         // TODO output data
 
         //printf("Waiting for data query from SNES...\n");
-        
-        //bool nRD = ((sio_hw->gpio_in) & 0x010000) >> 16;
-        //bool nCART = ((sio_hw->gpio_in) & 0x020000) >> 17;
-        //while (nRD || nCART) {
-        //uint8_t rd_cart = ((sio_hw->gpio_in) & 0x030000) >> 16;
-        //while((((sio_hw->gpio_in) & 0x030000) >> 16) != 0) {
-        while(((sio_hw->gpio_in) & 0x18000000) != 0) {
+        while((gpio_get_all64() & SNES_CTRL_PINS_MASK) != 0) {
              tight_loop_contents();
         }
-        uint32_t address = ((sio_hw->gpio_in) & 0xffff);    // FIXME 24 bit address with bank??
-        //printf("Data requested. Address=%04x\n", address);
+        uint32_t address = (gpio_get_all64() & SNES_ADDR_PINS_MASK);
+        //printf("Data requested. Address=%06x\n", address);
 
-        uint8_t data = (address & 0xf000) == 0xf000 ? rom[address & 0x7fff] : rom[address & 0x6fff];    // FIXME
+        uint32_t data_location_in_rom = map_address_to_rom(address);
+        uint8_t data = rom[data_location_in_rom]; //(address & 0xf000) == 0xf000 ? rom[address & 0x7fff] : rom[address & 0x6fff];    // FIXME
         //printf("Data=%02x\n", data);
 
-        // DATA pins mask: 0x04000000 & 0x007f0000
-        uint32_t data_out = ((data & 0x80) << 19) | ((data & 0x7f) << 16);
+        uint64_t data_out = data << SNES_DATA_PINS_SHIFT;
 
-        gpio_set_dir_out_masked(0x047f0000);
-        gpio_put_masked(0x047f0000, data_out);
+        gpio_set_dir_out_masked64(SNES_DATA_PINS_MASK);
+        gpio_put_masked64(SNES_DATA_PINS_MASK, data_out);
 
 #ifdef DEBUG
         addresses[(counter-1)%BUFFER_SIZE] = address ;//FIXME & 0x7fff;
@@ -97,20 +120,20 @@ int main() {
 #endif
 
         // Wait for /RD to go HIGH
-        while(((sio_hw->gpio_in) & 0x08000000) == 0) {
+        while((gpio_get_all64() & SNES_RD_PIN_MASK) == 0) {
             tight_loop_contents();
         }
 
         // FIXME when to clear data bus ??
 
         // TODO set as high-impedance ?!
-        gpio_set_dir_in_masked(0x047f0000);
-        //gpio_clr_mask(0x047f0000);
+        gpio_set_dir_in_masked64(SNES_DATA_PINS_MASK);
+        //gpio_clr_mask64(SNES_DATA_PINS_MASK);
     }
 
 #ifdef DEBUG
     for (int i=0; i<BUFFER_SIZE; i++) {
-        printf("#%02d: %04x -> rom[%04x] = %02x (%08x)\n", i, addresses[i], addresses[i] & 0x7fff, datas[i], datas_out[i]);
+        printf("#%02d: %06x -> rom[%06x] = %02x (%08x)\n", i, addresses[i], map_address_to_rom(addresses[i]), datas[i], datas_out[i]);
     }
 
     sleep_ms(5000);
